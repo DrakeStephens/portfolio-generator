4a400e0f1a23f99b751078f3d80987c76ebfe40d
Drake Stephens <drake.stephens56@gmail.com>
2020-06-24 01:54:58 -0600

    lesson2

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+const profileDataArgs = process.argv.slice(2)
+const [name, github] = profileDataArgs;
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw err;
 
-const printProfileData = profileDataArr => {
-
-    
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-    console.log('============');
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-
-  };
-
-
-printProfileData(profileDataArgs);
+    console.log('Portolio complete! check out index.html to see the output!');
+})
